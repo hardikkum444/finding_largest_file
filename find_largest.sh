ca980ad7d5ad81e268935c4328bf232d9315f61d
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2023-12-24 13:40:02 +0530

    shell script done

---
+++ find_largest.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if a directory is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 /path/to/pendrive"
+    exit 1
+fi
+
+# Ensure the provided path is a directory
+pendrive_path="$1"
+if [ ! -d "$pendrive_path" ]; then
+    echo "Error: $pendrive_path is not a directory."
+    exit 1
+fi
+
+echo "Finding the largest files in $pendrive_path ..."
+
+# Measure the time it takes to find the largest files
+start_time=$(date +%s.%N)
+largest_files=$(find "$pendrive_path" -type f -exec du -h {} + | sort -rh | head -n 10)
+end_time=$(date +%s.%N)
+
+# Display the largest files and the time it took
+echo -e "\nTop 10 largest files:"
+echo "$largest_files"
+echo -e "\nTime taken: $(echo "$end_time - $start_time" | bc) seconds"
+echo "Done."
